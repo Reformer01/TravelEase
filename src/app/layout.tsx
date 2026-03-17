084cd5aab6f887e7be2c5242472acf170ef2ea64
Reformer Ejembi <reformer.ejembi@iworldnetworks.net>
2026-03-17 07:08:45 +0000

    Try fixing this error: `Runtime Error: useFirebase must be used within a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-
 import type { Metadata } from 'next';
 import './globals.css';
 import { BasketProvider } from '@/context/basket-context';
 import { Navbar } from '@/components/layout/navbar';
 import { Toaster } from '@/components/ui/toaster';
+import { FirebaseClientProvider } from '@/firebase';
 
 export const metadata: Metadata = {
   title: 'VoyageFlow | Seamless Travel Planning',
@@ -22,14 +22,16 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased bg-background min-h-screen flex flex-col">
-        <BasketProvider>
-          <Navbar />
-          <main className="flex-1">
-            {children}
-          </main>
-          <Toaster />
-        </BasketProvider>
+      <body className="font-body antialiased bg-background min-h-screen flex flex-col" suppressHydrationWarning>
+        <FirebaseClientProvider>
+          <BasketProvider>
+            <Navbar />
+            <main className="flex-1">
+              {children}
+            </main>
+            <Toaster />
+          </BasketProvider>
+        </FirebaseClientProvider>
       </body>
     </html>
   );
